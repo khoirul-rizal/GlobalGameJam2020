b393748198aca3c643148367be4432d4ce8545cf
KhoTech <khoirulrizal322932@gmail.com>
2020-02-01 11:53:16 +0700

    first init

---
+++ Assets/ObjectInteraction.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class ObjectInteraction : MonoBehaviour
+{
+    
+    public Sprite[] bloodList;
+    SpriteRenderer spriteRenderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+        spriteRenderer.sprite = bloodList[Random.Range(0,bloodList.Length)];
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter2D(Collision2D collision2D)
+    {
+      Debug.Log("hellow1");
+    }
+
+}
---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    [SerializeField]
+    GameObject player;
+    Vector2 velocity;
+    BoxCollider2D boxCollider;
+
+    float speed = 5;
+    float walkAcceleration = 10;
+    float groundDeceleration = 5;
+    void Start()
+    {
+      // player = gameObject.GetComponent<GameObject>();
+      boxCollider = player.GetComponent<BoxCollider2D>();
+    }
+    void Update()
+    {
+        CharacterMovement();
+        CharacterInteraction();
+    }
+   
+    void CharacterMovement()
+    {
+      float moveInputX = Input.GetAxisRaw("Horizontal");
+      velocity.x = Mathf.MoveTowards(velocity.x, speed * moveInputX, walkAcceleration * Time.deltaTime);
+
+      if (moveInputX != 0)
+      {
+        velocity.x = Mathf.MoveTowards(velocity.x, speed * moveInputX, walkAcceleration * Time.deltaTime);
+      }
+      else
+      {
+          velocity.x = Mathf.MoveTowards(velocity.x, 0, groundDeceleration * Time.deltaTime);
+      }
+
+      
+      float moveInputY = Input.GetAxisRaw("Vertical");
+      velocity.y = Mathf.MoveTowards(velocity.y, speed * moveInputY, walkAcceleration * Time.deltaTime);
+
+      if (moveInputY != 0)
+      {
+        velocity.y = Mathf.MoveTowards(velocity.y, speed * moveInputY, walkAcceleration * Time.deltaTime);
+      }
+      else
+      {
+          velocity.y = Mathf.MoveTowards(velocity.y, 0, groundDeceleration * Time.deltaTime);
+      }
+
+
+      player.transform.Translate(velocity * Time.deltaTime);
+
+      Collider2D[] hits = Physics2D.OverlapBoxAll(player.transform.position, boxCollider.size+boxCollider.offset , 0);
+      foreach (Collider2D hit in hits)
+      {
+        if (hit.gameObject.tag == "Finish") return;
+        if (hit == boxCollider)
+          continue;
+
+        ColliderDistance2D colliderDistance = hit.Distance(boxCollider);
+
+        if(colliderDistance.isOverlapped)
+        {
+          transform.Translate(colliderDistance.pointA - colliderDistance.pointB);
+        }
+      }
+    }
+    void CharacterInteraction()
+    {
+      if(Input.GetButton("Interact")){
+
+      }
+    }
+    
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+      if(collision.gameObject.tag == "Finish")
+      Debug.Log("hellow12");
+    }
+
+    public Vector2 Velocity(){
+      return velocity;
+    }
+}
