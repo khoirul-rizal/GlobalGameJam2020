5e67adcf8e400ecee2b57b8cb242dd6669852ed0
hamilia94 <khoirulrizal322932@gmail.com>
2020-02-01 11:08:23 +0700

    first commit

---
+++ Assets/ObjectInteraction.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class ObjectInteraction : MonoBehaviour
+{
+    
+    public Sprite[] bloodList;
+    SpriteRenderer spriteRenderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+        spriteRenderer.sprite = bloodList[Random.Range(0,bloodList.Length)];
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+      // foreach (ContactPoint contact in collision.contacts){
+
+      // }
+      Debug.Log("hellow1");
+    }
+
+}
---
+++ Assets/Scripts/CharacterAnim.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class CharacterAnim : MonoBehaviour
+{
+    [SerializeField]
+    CharacterController characterController;
+    Animator animator;
+    float isFaceRight;
+
+    void Start()
+    {
+        animator = gameObject.GetComponent<Animator>();
+    }
+    void Update()
+    {
+        if(characterController.Velocity().x > 0.1) isFaceRight= 1;
+        if(characterController.Velocity().x < -0.1) isFaceRight= -1;
+
+        animator.SetFloat("isFaceRight", isFaceRight);
+        if(characterController.Velocity().x != 0 || characterController.Velocity().y != 0){
+            animator.SetBool("isWalk",true);
+        }else{
+            animator.SetBool("isWalk",false);
+        }
+    }
+}
